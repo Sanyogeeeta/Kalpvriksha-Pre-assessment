0677f09543f0713b07ea2d909a03b8bc093da023
Sanyogeeta <Sanyogitaad2003@gmail.com>
2024-12-21 19:54:07 +0530

    Handling Edge Cases

--- calculator.c
+++ calculator.c
@@ -1,117 +1,114 @@
 #include<stdio.h>
 #include<string.h>
-#include<ctype.h>
-#include<stdlib.h>
-//store number in stack
-int numStack[30];
+
+//store number and operator in stack
+int number[30];
 int topnum=-1;
-//store operators in stack
-char opStack[20];
+char operator[20];
 int topop=-1;
+
+//Check for Digit
+int IsDigit(char c){
+    int flag=0;
+    if(c>='0' && c<='9')
+        flag=1;
+    return flag;
+}
+
 //Perform arithmetic operation
-void calculate(){
-    int x,y,z,n;
+void PerformOperation(){
+    int num1,num2,result;
     char op;
-    //To check there are minimum two operands
-    if(topnum>=1){
-    x=numStack[topnum--];
-    y=numStack[topnum--];
-    op=opStack[topop--];
-    switch(op){
-        case '+':z=x+y;
-            break;
-        case '-':z=y-x;
-            break;
-        case '*':z=x*y;
-            break;
-        case '/':
-        //Division by zero check
-            if(x==0){
-                printf("Error: Division by zero.");
-                exit(0);
-            }
-            z=y/x;
-            break;
-    }
-    numStack[++topnum]=z;
+    if(topnum>=1 && topop>=0){
+        num1=number[topnum--];
+        num2=number[topnum--];
+        op=operator[topop--];
+        switch(op){
+            case '+':result=num2+num1;
+                break;
+            case '-':result=num2-num1;
+                break;
+            case '*':result=num2*num1;
+                break;
+            case '/':
+                if(num1==0){
+                    printf("Error: Division by zero.\n");
+                    return;
+                }
+                result=num2/num1;
+                break;
+        }
+        number[++topnum]=result;
     }
     else{
         topnum=-1;
         topop=-1;
-        printf("Error: Invalid expression(Missing operand.)");
+        printf("Error: Invalid expression(Missing operand.)\n");
+        return;
     }
 }
+
 //operator precedence
 int precedence(char c){
-if(c=='+'||c=='-')
-    return 1;
-else if(c=='*'||c=='/')
-    return 2;
+    int result;
+    if(c=='+'||c=='-')
+        result=1;
+    else if(c=='*'||c=='/')
+        result=2;
+    return result;
 }
-int evaluation(char exp[]){
+
+//Expression evaluation
+int Evaluation(char exp[]){
     int i;
     char c;
     for(i=0;i<strlen(exp)-1;i++){
-    c=exp[i];
-    //ignore the white space
-    if(c==' ')
-        continue;
-    //check for digit and add to number stack
-    else if(isdigit(c)){
-            //multinum calculation           
-            int x=0;
-            while(isdigit(c)){
-                x=x*10+(c-'0');
+        c=exp[i];
+        if(c==' ')
+            continue;
+        //check for digit and add to number stack
+        else if(IsDigit(c)){         
+            int num=0;
+            while(IsDigit(c)){
+                num=num*10+(c-'0');
                 i++;
                 if(i<strlen(exp))
                     c=exp[i];
                 else
                     break;
             }
-            //printf("%d\n",x);
-            numStack[++topnum]=x;
+            number[++topnum]=num;
             i--;
         }
-        //check for operators and add to operator stack
         else if(c=='+'||c=='-'||c=='*'||c=='/'){
-            while(!(topop==-1) && (precedence(c)<=precedence(opStack[topop]))){
-                calculate();
+            while(!(topop==-1) && (precedence(c)<=precedence(operator[topop]))){
+                PerformOperation();
             }
-            opStack[++topop]=c;
-            //printf("%c\n",opStack[topop]);
+            operator[++topop]=c;
         }
-        //Invalid inputs
         else{
-            printf("'Error: Invalid expression.");
-            exit(0);
+            return -999;
         }
     }
     while(topop!=-1)
-        calculate();
-    //if there is experssio error return -999
-    if(topnum==-1){
+        PerformOperation();
+    if((topop==-1 && topnum!=0)||topnum==-1)
         return -999;
-    }
-    return numStack[topnum];
+    return number[topnum];
 }
+
 int main(){
     //The user input for expression in string format
     char exp[50];
     printf("Enter Mathematical expression:");
     fgets(exp, sizeof(exp),stdin);
-    if(strlen(exp)==0||(strlen(exp)==1 && !(strcmp(" ",exp)))){
-    //Handle empty or just space expression
-        printf("Error: Invalid expression\n");
-        return 0;
-    }
-    else{
     int i, j = 0;
     int res=0;
-    res=evaluation(exp);
-    if(res==-999){
-        return 0;
+    res=Evaluation(exp);
+    if(strlen(exp)==0||(strlen(exp)==1 && !(strcmp(" ",exp)))||res==-999){
+        printf("Error: Invalid expression\n");
     }
     else
-    printf("The result is %d",res);
-    }
+        printf("The result is %d\n",res);
+    return 0;
 }
